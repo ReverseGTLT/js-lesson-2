98c98e202990306ca44ece808ad8242b20c5bd08
ReverseGTLT <dzewrpifzc@gmail.com>
2022-10-05 14:11:20 +0300

    Added js structure

---
+++ assets/js/main.js
@@ -0,0 +1,4 @@
+let numberFirst = prompt("Введите первое число", +'');
+let numberSecond = prompt('Введите второе число', +'');
+let result = Number(numberFirst) + Number(numberSecond);
+alert(result);
